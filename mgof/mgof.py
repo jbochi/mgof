7a5507b140f7c7412db7bdbcd59fe7aac86a5d64
Juarez Bochi <jbochi@gmail.com>
2015-02-15 16:59:36 -0200

    Use current timestamp as default

--- mgof/mgof.py
+++ mgof/mgof.py
@@ -7,11 +7,11 @@ class AnomalyDetector():
         self.r = redis.StrictRedis(host='localhost', port=6379)
 
     def _serialize_value(self, timestamp, value):
-        return "%d:%f" % (timestamp, value)
+        return "%f:%f" % (timestamp, value)
 
     def _read_value(self, string):
         ts, value = string.split(":")
-        return int(ts), float(value)
+        return float(ts), float(value)
 
     def post_metric(self, key, value, timestamp=None):
         timestamp = timestamp or time.time()
--- tests/test_mgof.py
+++ tests/test_mgof.py
@@ -1,22 +1,36 @@
 import mgof
+import pytest
 import redis
+import time
+
 
 TEST_KEY = "test_key"
 r = redis.StrictRedis(host='localhost', port=6379)
 
 
-def test_post_ang_get_metric_back():
-    a = mgof.AnomalyDetector()
+def setup_function(function):
     r.delete(TEST_KEY)
-    a.post_metric(key=TEST_KEY, value=40.0, timestamp=14000000)
 
+
+@pytest.fixture
+def a():
+    return mgof.AnomalyDetector()
+
+
+def test_post_ang_get_metric_back(a):
+    a.post_metric(key=TEST_KEY, value=40.0, timestamp=14000000)
     assert a.get_time_series(key=TEST_KEY) == [(14000000, 40.0)]
 
 
-def test_get_metric_should_return_values_in_specified_range():
+def test_should_use_current_timestamp_for_metric(a):
+    now = time.time()
+    a.post_metric(key=TEST_KEY, value=10.0)
+    assert abs(a.get_time_series(key=TEST_KEY)[0][0] - now) < 0.5
+
+
+def test_get_metric_should_return_values_in_specified_range(a):
     ts = 14000000
     a = mgof.AnomalyDetector()
-    r.delete(TEST_KEY)
     a.post_metric(key=TEST_KEY, value=40.0, timestamp=ts)
 
     assert a.get_time_series(key=TEST_KEY) == [(ts, 40.0)]
