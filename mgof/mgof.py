936f65ae2f74f140956374f6f78affc24e157315
Juarez Bochi <jbochi@gmail.com>
2015-02-15 16:38:57 -0200

    fix tests

--- mgof/mgof.py
+++ mgof/mgof.py
@@ -5,9 +5,16 @@ class AnomalyDetector():
     def __init__(self, host='localhost', port=6379):
         self.r = redis.StrictRedis(host='localhost', port=6379)
 
+    def _serialize_value(self, timestamp, value):
+        return "%d:%f" % (timestamp, value)
+
+    def _read_value(self, string):
+        ts, value = string.split(":")
+        return int(ts), float(value)
+
     def post_metric(self, key, value, timestamp=None):
         timestamp = timestamp or time.time()
-        return self.r.zadd(key, timestamp, ("%d:%f" % (timestamp, value)))
+        return self.r.zadd(key, timestamp, self._serialize_value(timestamp, value))
 
-    def get_metric(self, key, min_value="-inf", max_value="+inf"):
-        return self.r.zcount(key, min_value, max_value)
+    def get_metric(self, key, start="-inf", stop="+inf"):
+        return map(self._read_value, self.r.zrangebyscore(key, start, stop))
--- tests/test_mgof.py
+++ tests/test_mgof.py
@@ -9,7 +9,8 @@ def test_post_ang_get_metric_back():
     a = mgof.AnomalyDetector()
     r.delete(TEST_KEY)
     a.post_metric(key=TEST_KEY, value=40.0, timestamp=14000000)
-    a.get_metric(key=TEST_KEY) == [(14000000, 40.0)]
+
+    assert a.get_metric(key=TEST_KEY) == [(14000000, 40.0)]
 
 
 def test_get_metric_should_return_values_in_specified_range():
@@ -18,8 +19,8 @@ def test_get_metric_should_return_values_in_specified_range():
     r.delete(TEST_KEY)
     a.post_metric(key=TEST_KEY, value=40.0, timestamp=ts)
 
-    a.get_metric(key=TEST_KEY) == [(ts, 40.0)]
-    a.get_metric(key=TEST_KEY, min_value=ts - 10, max_value=ts + 10) == [(ts, 40.0)]
-    a.get_metric(key=TEST_KEY, min_value="-inf", max_value="+inf") == [(ts, 40.0)]
-    a.get_metric(key=TEST_KEY, min_value=ts - 20, max_value=ts - 10) == []
-    a.get_metric(key=TEST_KEY, min_value=ts + 10, max_value=ts - 20) == []
+    assert a.get_metric(key=TEST_KEY) == [(ts, 40.0)]
+    assert a.get_metric(key=TEST_KEY, start=ts - 10, stop=ts + 10) == [(ts, 40.0)]
+    assert a.get_metric(key=TEST_KEY, start="-inf", stop="+inf") == [(ts, 40.0)]
+    assert a.get_metric(key=TEST_KEY, start=ts - 20, stop=ts - 10) == []
+    assert a.get_metric(key=TEST_KEY, start=ts + 10, stop=ts - 20) == []
