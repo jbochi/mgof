538070f54f2182800caf8f144e67cd399aa30384
Juarez Bochi <jbochi@gmail.com>
2015-03-01 14:19:37 -0300

    Update script to post load

--- util/get_load_mac.sh
+++ util/get_load_mac.sh
@@ -1,1 +1,8 @@
-while true; do echo "ZADD load `date +'%s'` `date +'%s'`:`sysctl -n vm.loadavg | awk '{print $2}'`" |  xargs -t redis-cli; sleep 1; done
+while true
+LOAD=`sysctl -n vm.loadavg | awk '{print $2}'`
+
+do
+    python -c "import mgof; a = mgof.AnomalyDetector(); a.post_metric('load', $LOAD)"
+    echo $LOAD
+    sleep 1
+done
