673967d75839ce0e6bbe7af5bf759ce80d3e8280
Juarez Bochi <jbochi@gmail.com>
2015-03-02 14:29:48 -0300

    more options for anomaly detector util

--- util/anomaly_detector.py
+++ util/anomaly_detector.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import argparse
 import os
 import sys
 
@@ -7,15 +8,39 @@ sys.path.append(path)
 
 import mgof
 
+parser = argparse.ArgumentParser(description='Detect anomaly')
+parser.add_argument('key', type=str,
+                   help='redis key')
+parser.add_argument('--redis_host', "-r", type=str, default="localhost",
+                    help='redis host')
+parser.add_argument('--redis_port', "-p", type=int, default=6379,
+                   help='redis port')
+parser.add_argument('--socket_timeout', "-t", type=int, default=30,
+                    help='redis socket timeout')
+parser.add_argument('--n_bins', "-n", type=int, default=10,
+                   help='number of bins')
+parser.add_argument('--min_value', "-i", type=int, default=0,
+                   help='min value of time series')
+parser.add_argument('--max_value', "-x", type=int, default=1,
+                   help='min value of time series')
+parser.add_argument('--window_size', "-w", type=int, default=60,
+                   help='window size')
+parser.add_argument('--confidence', "-c", type=int, default=95,
+                   help='confidence level (95 or 99)')
+
+
 def main(key):
-    a = mgof.AnomalyDetector()
-    print("Valid interval (tukey): ", a.tukey_range(key))
-    anomaly = a.is_window_anomalous(key,
-        n_bins=10,
-        min_value=0,
-        max_value=100,
-        window_size=10*60,
-        confidence=95)
+    args = parser.parse_args()
+    detector = mgof.AnomalyDetector(
+        host=args.redis_host,
+        port=args.redis_port,
+        socket_timeout=args.socket_timeout)
+    anomaly = detector.is_window_anomalous(key,
+        n_bins=args.n_bins,
+        min_value=args.min_value,
+        max_value=args.max_value,
+        window_size=args.window_size,
+        confidence=args.confidence)
     print("Anomaly (confidence 95%): ", anomaly)
 
 if __name__ == "__main__":
